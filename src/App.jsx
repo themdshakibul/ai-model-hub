9d345c174d4ca97757d96b1f7a2611990ce70af4
themdshakibul <mdshakibul88240@gmail.com>
2026-03-28 11:11:29 +0600

    Banner Sectioon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Pages/Banner";
 
 function App() {
   return (
     <>
       <div>
         <NavBar />
+        <Banner />
         <Footer />
       </div>
     </>
